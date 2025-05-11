0d06a4ec84f90d83c54c473a43cb93d6b6fa8393
iismail19 <2015.ismail@gmail.com>
2025-05-10 18:13:39 -0700

    Refactor server.js code

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const axios = require("axios");
 const cors = require("cors");
 require("dotenv").config();
 
-// get values from env
+// Log API key for debugging
 console.log(process.env.API_KEY);
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// Use CORS middleware - solve cors issues blocking react from making requests to server
-app.use(cors());
+app.use(cors()); // Solve CORS issues
 
 const PORT = process.env.PORT || 5005;
 
@@ -18,94 +17,64 @@ const BASE_URL = "https://americas.api.riotgames.com";
 const MATCH_LIST_URL = "/lol/match/v5/matches/by-puuid/";
 const MATCH_URL = "/lol/match/v5/matches/";
 const GET_ACCOUNT_BY_SUMMONER_NAME = "/riot/account/v1/accounts/by-riot-id/";
-const API_KEY = "api_key=" + process.env.API_KEY;
+const API_KEY = `api_key=${process.env.API_KEY}`;
 
-function getByRiotIdURL(body) {
-  const urlFriendlyGameName = encodeURIComponent(body.gameName);
-  const tagline = body.tagline;
-  const url =
-    BASE_URL +
-    GET_ACCOUNT_BY_SUMMONER_NAME +
-    urlFriendlyGameName +
-    "/" +
-    tagline +
-    "?" +
-    API_KEY;
-  return url;
-}
+// Helper functions for constructing URLs
+const getByRiotIdURL = ({ gameName, tagline }) =>
+  `${BASE_URL}${GET_ACCOUNT_BY_SUMMONER_NAME}${encodeURIComponent(
+    gameName
+  )}/${tagline}?${API_KEY}`;
 
-function getMatchesURL(body) {
-  const url =
-    BASE_URL + MATCH_LIST_URL + body + "/" + "ids?start=0&count=20&" + API_KEY;
-  return url;
-}
+const getMatchesURL = (puuid) =>
+  `${BASE_URL}${MATCH_LIST_URL}${puuid}/ids?start=0&count=20&${API_KEY}`;
 
-//https://americas.api.riotgames.com/lol/match/v5/matches/NA1_5180785472?api_key=
-function getMatchDataURL(matchId) {
-  const url = BASE_URL + MATCH_URL + matchId + "?" + API_KEY;
-  return url;
-}
+const getMatchDataURL = (matchId) =>
+  `${BASE_URL}${MATCH_URL}${matchId}?${API_KEY}`;
 
-// Get user puuid by gameName and tagLine
+// Get user PUUID by gameName and tagLine
 app.post("/", async (req, res) => {
   try {
-    // console.log(req.body);
-    // get puuid
     const url = getByRiotIdURL(req.body);
     const response = await axios.get(url);
-    console.log(response.data);
 
-    //get matches
     const matchesURL = getMatchesURL(response.data.puuid);
     const listOfMatches = await axios.get(matchesURL);
-    console.log(listOfMatches.data);
 
-    // get data for each mathch add add that to a map with match_id and retreived data
-    // todo remove
-    let matchDataList = [];
-    for (let i = 0; i < listOfMatches.data.length; i++) {
-      const matchId = listOfMatches.data[i];
-      const matchDataURL = getMatchDataURL(matchId);
-      const matchData = await axios.get(matchDataURL);
-      console.log(matchData.data);
-      matchDataList.push(matchData.data);
-    }
+    const matchDataList = await Promise.all(
+      listOfMatches.data.map(async (matchId) => {
+        const matchDataURL = getMatchDataURL(matchId);
+        const matchData = await axios.get(matchDataURL);
+        return matchData.data;
+      })
+    );
 
-    const matchResponse = {
+    res.json({
       puuid: response.data.puuid,
-      matchDataList: matchDataList,
-    };
-
-    res.json(matchResponse); // Send the response to the client
+      matchDataList,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({
-      error:
-        "Error fetching puuid by gameName and tagLine via by-riot-id api call",
+      error: "Error fetching data via Riot API",
     });
   }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 // Request for match list
-
 app.post("/matches", async (req, res) => {
   try {
-    console.log(req.body.puuid);
     const url = getMatchesURL(req.body.puuid);
     const response = await axios.get(url);
-    console.log(response.data); // Print to the console
-    res.json(response.data); // Send the response to the client
+    res.json(response.data);
   } catch (error) {
     console.error(error);
     res.status(500).json({
-      error:
-        "Error fetching puuid by gameName and tagLine via by-riot-id api call",
+      error: "Error fetching match list via Riot API",
     });
   }
 });
 
-// Get all matches from a single request
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
